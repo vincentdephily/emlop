cc8fa62c0c57be9ef39c260848dc1b1e0d822603
Vincent de Phily <moltonel@gmail.com>
2018-02-13 23:04:47 +0000

    Fix test/generate.sh and update test data.
    emerge.all.log was generated badly so that none of the emerges
    completed. The test was still very useful, but it shows that we
    should tighten the checks.

--- test/generate.sh
+++ test/generate.sh
@@ -9,9 +9,9 @@ find /usr/portage -name '*.ebuild'|sed -r s/.ebuild$//|cut -d/ -f4,5|sort -u > c
 echo "Generating an emerge log of all ebuilds all versions in $PWD/emerge.all.log"
 rm emerge.all.log
 d=$(date -d '2017-01-01T00:00:00Z' +%s)
-for i in $(find /usr/portage -name '*.ebuild'|sed -r s/.ebuild$//|cut -d/ -f4,6|sort -u); do
+for i in $(find /usr/portage -name '*.ebuild'|sed -r s/.ebuild$//|cut -d/ -f4,6|grep -v ^skel$|sort -u); do
     echo "$d: >>> emerge (1 of 1) $i to /" >> emerge.all.log
     let d++
-    echo "$d::: completed emerge (1 of 7) $i to /" >> emerge.all.log
+    echo "$d:  ::: completed emerge (1 of 7) $i to /" >> emerge.all.log
     let d++
 done
