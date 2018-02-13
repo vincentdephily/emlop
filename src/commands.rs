d81f682b1aee2ac368557a35e47991b23ca3ce55
Vincent de Phily <vincent.dephily@mobile-devices.fr>
2018-02-13 16:04:48 +0000

    Fix 'summary' subcommand.
    This was broken by 8ae1d5a32d371ed9f4d4d44356485896f576e6d0, we need a unittest ;) Or use StructOpt,
    which should catch this at compile-time. No point in delaying this fix though.

--- src/commands.rs
+++ src/commands.rs
@@ -35,7 +35,7 @@ pub fn cmd_list(args: &ArgMatches, subargs: &ArgMatches) -> Result<(), io::Error
 /// Then we compute the stats per ebuild, and print that.
 pub fn cmd_summary(args: &ArgMatches, subargs: &ArgMatches) -> Result<(), io::Error> {
     let parser = HistParser::new(args.value_of("logfile").unwrap(), subargs.value_of("package"));
-    let lim = value_t!(args, "limit", usize).unwrap();
+    let lim = value_t!(subargs, "limit", usize).unwrap();
     let mut started: HashMap<(String, String, String), i64> = HashMap::new();
     let mut times: HashMap<String, Vec<i64>> = HashMap::new();
     let mut maxlen = 0;

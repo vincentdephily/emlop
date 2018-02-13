vincentdephily/emlop

commit 8ae1d5a32d371ed9f4d4d44356485896f576e6d0

Refactor passing of CLI arguments from main() to cmd_*().
This'll make future additions of CLI flags easyer.
Might switch to StructOpt crate at some stage to reduce boilerplate.

src/commands.rs
@@ -9,8 +9,8 @@ use proces::*;
 /// Straightforward display of merge events
 ///
 /// We store the start times in a hashmap to compute/print the duration when we reach a stop event.
-pub fn cmd_list(filename: &str, args: &ArgMatches) -> Result<(), io::Error> {
-    let hist = HistParser::new(filename, args.value_of("package"));
+pub fn cmd_list(args: &ArgMatches, subargs: &ArgMatches) -> Result<(), io::Error> {
+    let hist = HistParser::new(args.value_of("logfile").unwrap(), subargs.value_of("package"));
     let mut started: HashMap<(String,String,String), i64> = HashMap::new();
     for event in hist {
         match event {
@@ -33,8 +33,8 @@ pub fn cmd_list(filename: &str, args: &ArgMatches) -> Result<(), io::Error> {
 ///
 /// First loop is like cmd_list but we store the merge time for each ebuild instead of printing it.
 /// Then we compute the stats per ebuild, and print that.
-pub fn cmd_summary(filename: &str, args: &ArgMatches) -> Result<(), io::Error> {
-    let parser = HistParser::new(filename, args.value_of("package"));
+pub fn cmd_summary(args: &ArgMatches, subargs: &ArgMatches) -> Result<(), io::Error> {
+    let parser = HistParser::new(args.value_of("logfile").unwrap(), subargs.value_of("package"));
     let lim = value_t!(args, "limit", usize).unwrap();
     let mut started: HashMap<(String,String,String), i64> = HashMap::new();
     let mut times: HashMap<String, Vec<i64>> = HashMap::new();
@@ -70,11 +70,12 @@ pub fn cmd_summary(filename: &str, args: &ArgMatches) -> Result<(), io::Error> {
 /// Predict future merge time
 ///
 /// Very similar to cmd_summary except we want total build time for a list of ebuilds parsed from stdin.
-pub fn cmd_predict(filename: &str, args: &ArgMatches) -> Result<(), io::Error> {
+pub fn cmd_predict(args: &ArgMatches, subargs: &ArgMatches) -> Result<(), io::Error> {
     let now = epoch_now();
-    let hist = HistParser::new(filename, None);
+    let cms = current_merge_start();
+    let hist = HistParser::new(args.value_of("logfile").unwrap(), None);
     let pretend = PretendParser::new();
-    let lim = value_t!(args, "limit", usize).unwrap();
+    let lim = value_t!(subargs, "limit", usize).unwrap();
     let mut started: HashMap<(String,String), i64> = HashMap::new();
     let mut times: HashMap<String, Vec<i64>> = HashMap::new();
     let mut maxlen = 0;
@@ -96,7 +97,6 @@ pub fn cmd_predict(filename: &str, args: &ArgMatches) -> Result<(), io::Error> {
             }
         }
     }
-    let cms = current_merge_start();
     let mut tottime = 0;
     let mut totcount = 0;
     let mut totunknown = 0;

src/main.rs
@@ -49,9 +49,9 @@ fn main() {
         .get_matches();
 
     match args.subcommand() {
-        ("list",    Some(sub_args)) => cmd_list(args.value_of("logfile").unwrap(), sub_args),
-        ("summary", Some(sub_args)) => cmd_summary(args.value_of("logfile").unwrap(), sub_args),
-        ("predict", Some(sub_args)) => cmd_predict(args.value_of("logfile").unwrap(), sub_args),
+        ("list",    Some(sub_args)) => cmd_list(&args, sub_args),
+        ("summary", Some(sub_args)) => cmd_summary(&args, sub_args),
+        ("predict", Some(sub_args)) => cmd_predict(&args, sub_args),
         (other, _) => unimplemented!("{} subcommand", other),
     }.unwrap();
 }

commit d81f682b1aee2ac368557a35e47991b23ca3ce55

Fix 'summary' subcommand.
This was broken by 8ae1d5a32d371ed9f4d4d44356485896f576e6d0, we need a unittest ;) Or use StructOpt,
which should catch this at compile-time. No point in delaying this fix though.

src/commands.rs
@@ -35,7 +35,7 @@ pub fn cmd_list(args: &ArgMatches, subargs: &ArgMatches) -> Result<(), io::Error
 /// Then we compute the stats per ebuild, and print that.
 pub fn cmd_summary(args: &ArgMatches, subargs: &ArgMatches) -> Result<(), io::Error> {
     let parser = HistParser::new(args.value_of("logfile").unwrap(), subargs.value_of("package"));
-    let lim = value_t!(args, "limit", usize).unwrap();
+    let lim = value_t!(subargs, "limit", usize).unwrap();
     let mut started: HashMap<(String, String, String), i64> = HashMap::new();
     let mut times: HashMap<String, Vec<i64>> = HashMap::new();
     let mut maxlen = 0;