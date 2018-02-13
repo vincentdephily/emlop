10d6f0de545dc8c68cacd9914cc001e1b9a1db04
Vincent de Phily <vincent.dephily@mobile-devices.fr>
2018-02-13 14:51:25 +0000

    Remove leading 0 from durations.
    Just looks nicer.

--- src/main.rs
+++ src/main.rs
@@ -68,9 +68,9 @@ pub fn fmt_duration(secs: i64) -> String {
     let h = (secs / 3600).abs();
     let m = (secs % 3600 / 60).abs();
     let s = (secs % 60).abs();
-    if h > 0      { format!("{}{:02}:{:02}:{:02}", neg, h, m, s) }
-    else if m > 0 { format!(      "{}{:02}:{:02}", neg, m, s) }
-    else          { format!(            "{}{:02}", neg, s) }
+    if h > 0      { format!("{}{}:{:02}:{:02}", neg, h, m, s) }
+    else if m > 0 { format!(      "{}{}:{:02}", neg, m, s) }
+    else          { format!(            "{}{}", neg, s) }
 }
 
 pub fn fmt_time(ts: i64) -> DateTime<Local> {
@@ -92,17 +92,17 @@ mod tests {
 
     #[test]
     fn duration() {
-        assert_eq!(       "00", fmt_duration(0));
-        assert_eq!(       "01", fmt_duration(1));
+        assert_eq!(        "0", fmt_duration(0));
+        assert_eq!(        "1", fmt_duration(1));
         assert_eq!(       "59", fmt_duration(59));
-        assert_eq!(    "01:00", fmt_duration(60));
-        assert_eq!(    "01:01", fmt_duration(61));
+        assert_eq!(     "1:00", fmt_duration(60));
+        assert_eq!(     "1:01", fmt_duration(61));
         assert_eq!(    "59:59", fmt_duration(3599));
-        assert_eq!( "01:00:00", fmt_duration(3600));
+        assert_eq!(  "1:00:00", fmt_duration(3600));
         assert_eq!( "99:59:59", fmt_duration(359999));
         assert_eq!("100:00:00", fmt_duration(360000));
-        assert_eq!(      "-01", fmt_duration(-1));
-        assert_eq!(   "-01:00", fmt_duration(-60));
-        assert_eq!("-01:00:00", fmt_duration(-3600));
+        assert_eq!(       "-1", fmt_duration(-1));
+        assert_eq!(    "-1:00", fmt_duration(-60));
+        assert_eq!( "-1:00:00", fmt_duration(-3600));
     }
 }
