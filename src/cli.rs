ecda5322935619a905987aab120e5557ea36119f
Vincent de Phily <moltonel@gmail.com>
2022-02-12 17:03:27 +0000

    cli: Tweak/reformat help strings

--- src/cli.rs
+++ src/cli.rs
@@ -9,13 +9,16 @@ pub fn build_cli_nocomplete() -> App<'static, 'static> {
                                .help("Use the last N merge times to predict next merge time.");
     let arg_pkg =
         Arg::with_name("package").takes_value(true).help("Show only packages matching <package>.");
-    let arg_exact = Arg::with_name("exact")
-        .short("e")
-        .long("exact")
-        .help("Match package with a string instead of a regex.")
-        .long_help("Match package with a string instead of a regex. \
-Regex is case-insensitive and matches on category/name (see https://docs.rs/regex/1.1.0/regex/#syntax). \
-String is case-sentitive and matches on whole name, or whole category/name if it contains a /."); //FIXME auto crate version
+    let arg_exact =
+        Arg::with_name("exact").short("e")
+                               .long("exact")
+                               .help("Match package with a string instead of a regex.")
+                               .long_help(
+                                          "Match package with a string instead of a regex. \
+                    Regex is case-insensitive and matches on category/name (see \
+                    https://docs.rs/regex/*/regex/#syntax). String is case-sentitive and \
+                    matches on whole name, or whole category/name if it contains a /.",
+        );
     let arg_show_l = Arg::with_name("show")
         .short("s")
         .long("show")
@@ -23,7 +26,8 @@ String is case-sentitive and matches on whole name, or whole category/name if it
         .validator(|s| find_invalid("musa", &s))
         .default_value("m")
         .help("Show (m)erges, (u)nmerges, (s)yncs, and/or (a)ll.")
-        .long_help("Show individual (m)erges, (u)nmerges, portage tree (s)yncs, or (a)ll of these (any letters combination).");
+        .long_help("Show individual (m)erges, (u)nmerges, portage tree (s)yncs, \
+                    or (a)ll of these (any letters combination).");
     let arg_show_s = Arg::with_name("show")
         .short("s")
         .long("show")
@@ -31,16 +35,20 @@ String is case-sentitive and matches on whole name, or whole category/name if it
         .validator(|s| find_invalid("ptsa", &s))
         .default_value("p")
         .help("Show (p)ackages, (t)otals, (s)yncs, and/or (a)ll.")
-        .long_help("Show per-(p)ackage merges/unmerges, (t)otal merges/unmerges, portage tree (s)yncs, or (a)ll of these (any letters combination).");
-    let arg_group = Arg::with_name("group")
-        .short("g")
-        .long("groupby")
-        .value_name("y,m,w,d")
-        .possible_values(&["y","m","w","d"])
-        .hide_possible_values(true)
-        .help("Group by (y)ear, (m)onth, (w)eek, or (d)ay.")
-        .long_help("Group by (y)ear, (m)onth, (w)eek, or (d)ay.\n\
-The grouping key is displayed in the first column. Weeks start on monday and are formated as 'year-weeknumber'.");
+        .long_help("Show per-(p)ackage merges/unmerges, (t)otal merges/unmerges, \
+                    portage tree (s)yncs, or (a)ll of these (any letters combination).");
+    let arg_group =
+        Arg::with_name("group").short("g")
+                               .long("groupby")
+                               .value_name("y,m,w,d")
+                               .possible_values(&["y", "m", "w", "d"])
+                               .hide_possible_values(true)
+                               .help("Group by (y)ear, (m)onth, (w)eek, or (d)ay.")
+                               .long_help(
+                                          "Group by (y)ear, (m)onth, (w)eek, or (d)ay.\n\
+                    The grouping key is displayed in the first column. \
+                    Weeks start on monday and are formated as 'year-weeknumber'.",
+        );
     App::new("emlop")
         .version(crate_version!())
         .global_setting(AppSettings::ColoredHelp)
@@ -50,14 +58,17 @@ The grouping key is displayed in the first column. Weeks start on monday and are
         .setting(AppSettings::InferSubcommands)
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .setting(AppSettings::VersionlessSubcommands)
-        .about("A fast, accurate, ergonomic EMerge LOg Parser.\nhttps://github.com/vincentdephily/emlop")
+        .about("A fast, accurate, ergonomic EMerge LOg Parser.\n\
+                https://github.com/vincentdephily/emlop")
         .after_help("Subcommands can be abbreviated down to a single letter.\n\
-Exit code is 0 if sucessful, 1 in case of errors (bad argument...), 2 if search found nothing.")
-        .help_message("Show short (-h) or detailed (--help) help. Use <subcommand> -h/--help for subcommand help.")
+                     Subcommands have their own -h / --help.\n\
+                     Exit code is 0 if sucessful, 1 in case of errors (bad argument...), \
+                     2 if search found nothing.")
+        .help_message("Show short (-h) or detailed (--help) help.")
         .arg(Arg::with_name("utc")
              .long("utc")
              .global(true)
-             .help("Parse naive dates as UTC instead of local time"))
+             .help("Parse/display dates in UTC instead of local time"))
         .arg(Arg::with_name("from")
              .value_name("date")
              .short("f")
@@ -66,22 +77,31 @@ Exit code is 0 if sucessful, 1 in case of errors (bad argument...), 2 if search 
              .takes_value(true)
              .help("Only parse log entries after <date>.")
              .long_help("Only parse log entries after <date>.\n\
-Accepts formats like '2018-03-04', '2018-03-04 12:34:56', '2018-03-04T12:34', '1 year, 2 months', '10d', and unix timestamps."))
+                         Accepts formats like '2018-03-04', '2018-03-04 12:34:56', \
+                         '2018-03-04T12:34', '1 year, 2 months', '10d', and unix timestamps."))
         .arg(Arg::with_name("to")
              .value_name("date")
              .short("t")
              .long("to")
              .global(true)
              .takes_value(true)
-             .help("Only parse log entries before <date>."))
+             .help("Only parse log entries before <date>.")
+             .long_help("Only parse log entries before <date>.\n\
+                         Accepts formats like '2018-03-04', '2018-03-04 12:34:56', \
+                         '2018-03-04T12:34', '1 year, 2 months', '10d', and unix timestamps."))
         .arg(Arg::with_name("duration")
-             .value_name("hms,hms_fixed,s")
+             .value_name("format")
              .long("duration")
              .global(true)
              .possible_values(&["hms","hms_fixed","s"])
              .hide_possible_values(true)
              .default_value("hms")
-             .help("Format durations in hours:minutes:seconds, hours:minutes:seconds with leading zeros, or seconds."))
+             .help("Output durations in different formats.")
+             .long_help("Output durations in different formats.
+    hms:         10:30
+    hms_fixed: 0:10:30
+    s:             630
+"))
         .arg(Arg::with_name("date")
              .value_name("format")
              .long("date")
@@ -89,7 +109,16 @@ Accepts formats like '2018-03-04', '2018-03-04 12:34:56', '2018-03-04T12:34', '1
              .possible_values(&["ymd","d","ymdhms","dt","ymdhmso","dto","rfc3339","3339","rfc2822","2822","compact","unix"])
              .hide_possible_values(true)
              .default_value("ymdhms")
-             .help("Output dates in different formats (get the list with `--date help`)."))
+             .help("Output dates in different formats.")
+             .long_help("Output dates in different formats.
+    ymd|d:        2022-01-31
+    ymdhms|dt:    2022-01-31 08:59:46
+    ymdhmso|dto:  2022-01-31 08:59:46 +00:00
+    rfc3339|3339: 2022-01-31T08:59:46+00:00
+    rfc2822|2822: Mon, 31 Jan 2022 08:59:46 +00:00
+    compact:      20220131085946
+    unix:         1643619586
+"))
         .arg(Arg::with_name("logfile")
              .value_name("file")
              .long("logfile")
@@ -102,7 +131,11 @@ Accepts formats like '2018-03-04', '2018-03-04 12:34:56', '2018-03-04T12:34', '1
              .short("v")
              .global(true)
              .multiple(true)
-             .help("Show warnings (-v), info (-vv) and debug (-vvv) messages (errors are always displayed)."))
+             .help("Increase verbosity (can be given multiple times).")
+             .long_help("Increase verbosity (defaults to errors only)
+    -v:      show warnings
+    -vv:     show info
+    -vvv:    show debug"))
         .arg(Arg::with_name("color")
              .long("color").alias("colour")
              .global(true)
@@ -130,10 +163,13 @@ Accepts formats like '2018-03-04', '2018-03-04 12:34:56', '2018-03-04T12:34', '1
                     .arg(&arg_limit))
         .subcommand(SubCommand::with_name("stats")
                     .about("Show statistics about sucessful merges, unmerges and syncs.")
-                    .long_about("Show statistics about sucessful (un)merges (overall or per package) and syncs.\n\
-* <package>: merge count, total merge time, predicted merge time, unmerge count, total unmerge time, predicted unmerge time.\n\
-* Total:     merge count, total merge time, average merge time,   unmerge count, total unmerge time, average unmerge time.\n\
-* Sync:      sync count,  total sync time,  predicted sync time.")
+                    .long_about("Show statistics about sucessful (un)merges (overall or per \
+                                 package) and syncs.\n\
+                                 * <package>: merge count, total merge time, predicted merge time, \
+                                 unmerge count, total unmerge time, predicted unmerge time.\n\
+                                 * Total:     merge count, total merge time, average merge time,   \
+                                 unmerge count, total unmerge time, average unmerge time.\n\
+                                 * Sync:      sync count,  total sync time,  predicted sync time.")
                     .help_message("Show short (-h) or detailed (--help) help.")
                     .arg(&arg_show_s)
                     .arg(&arg_group)
