9a4096492908adb69052e93c62e2aa3f316537d9
Vincent de Phily <moltonel@gmail.com>
2024-02-06 20:48:07 +0000

    qa: s/args/cli/

--- src/config.rs
+++ src/config.rs
@@ -74,8 +74,8 @@ pub struct ConfAccuracy {
 
 impl Configs {
     pub fn load() -> Result<Configs, Error> {
-        let args = cli::build_cli().get_matches();
-        let level = match args.get_count("verbose") {
+        let cli = cli::build_cli().get_matches();
+        let level = match cli.get_count("verbose") {
             0 => LevelFilter::Error,
             1 => LevelFilter::Warn,
             2 => LevelFilter::Info,
@@ -83,11 +83,11 @@ impl Configs {
             _ => LevelFilter::Trace,
         };
         env_logger::Builder::new().filter_level(level).format_timestamp(None).init();
-        trace!("{:?}", args);
-        let toml = Toml::load(args.get_one::<String>("config"), var("EMLOP_CONFIG").ok())?;
+        trace!("{:?}", cli);
+        let toml = Toml::load(cli.get_one::<String>("config"), var("EMLOP_CONFIG").ok())?;
         log::trace!("{:?}", toml);
-        let conf = Conf::try_new(&args, &toml)?;
-        Ok(match args.subcommand() {
+        let conf = Conf::try_new(&cli, &toml)?;
+        Ok(match cli.subcommand() {
             Some(("log", sub)) => Self::Log(conf, ConfLog::try_new(sub, &toml)?),
             Some(("stats", sub)) => Self::Stats(conf, ConfStats::try_new(sub, &toml)?),
             Some(("predict", sub)) => Self::Predict(conf, ConfPred::try_new(sub, &toml)?),
@@ -101,17 +101,17 @@ impl Configs {
 // TODO nicer way to specify src
 fn sel<T, A, R>(cli: Option<&String>,
                 toml: Option<&T>,
-                argsrc: &'static str,
+                clisrc: &'static str,
                 tomlsrc: &'static str,
-                parg: A,
+                arg: A,
                 def: R)
                 -> Result<R, ArgError>
     where R: ArgParse<String, A> + ArgParse<T, A>
 {
     if let Some(a) = cli {
-        R::parse(a, parg, argsrc)
+        R::parse(a, arg, clisrc)
     } else if let Some(a) = toml {
-        R::parse(a, parg, tomlsrc)
+        R::parse(a, arg, tomlsrc)
     } else {
         Ok(def)
     }
@@ -138,26 +138,26 @@ macro_rules! sel {
 
 
 impl Conf {
-    pub fn try_new(args: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
+    pub fn try_new(cli: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
         let isterm = std::io::stdout().is_terminal();
-        let color = match args.get_one("color") {
+        let color = match cli.get_one("color") {
             Some(ColorStyle::Always) => true,
             Some(ColorStyle::Never) => false,
             None => isterm,
         };
-        let out = match args.get_one("output") {
+        let out = match cli.get_one("output") {
             Some(o) => *o,
             None if isterm => OutStyle::Columns,
             None => OutStyle::Tab,
         };
-        let header = args.get_flag("header");
-        let dur_t = *args.get_one("duration").unwrap();
-        let offset = get_offset(args.get_flag("utc"));
-        Ok(Self { logfile: sel!(args, toml, logfile, (), String::from("/var/log/emerge.log"))?,
-                  from: args.get_one("from")
-                            .map(|d| i64::parse(d, offset, "--from"))
-                            .transpose()?,
-                  to: args.get_one("to").map(|d| i64::parse(d, offset, "--to")).transpose()?,
+        let header = cli.get_flag("header");
+        let dur_t = *cli.get_one("duration").unwrap();
+        let offset = get_offset(cli.get_flag("utc"));
+        Ok(Self { logfile: sel!(cli, toml, logfile, (), String::from("/var/log/emerge.log"))?,
+                  from: cli.get_one("from")
+                           .map(|d| i64::parse(d, offset, "--from"))
+                           .transpose()?,
+                  to: cli.get_one("to").map(|d| i64::parse(d, offset, "--to")).transpose()?,
                   pkg: AnsiStr::from(if color { "\x1B[1;32m" } else { "" }),
                   merge: AnsiStr::from(if color { "\x1B[1;32m" } else { ">>> " }),
                   unmerge: AnsiStr::from(if color { "\x1B[1;31m" } else { "<<< " }),
@@ -168,65 +168,65 @@ impl Conf {
                   header,
                   dur_t,
                   date_offset: offset,
-                  date_fmt: sel!(args, toml, date, (), DateStyle::default())?,
+                  date_fmt: sel!(cli, toml, date, (), DateStyle::default())?,
                   out })
     }
     #[cfg(test)]
     pub fn from_str(s: impl AsRef<str>) -> Self {
-        let args = cli::build_cli().get_matches_from(s.as_ref().split_whitespace());
-        Self::try_new(&args, &Toml::default()).unwrap()
+        let cli = cli::build_cli().get_matches_from(s.as_ref().split_whitespace());
+        Self::try_new(&cli, &Toml::default()).unwrap()
     }
 }
 
 impl ConfLog {
-    fn try_new(args: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
-        Ok(Self { show: sel!(args, toml, log, show, "musa", Show::m())?,
-                  search: args.get_many("search").unwrap_or_default().cloned().collect(),
-                  exact: args.get_flag("exact"),
-                  starttime: sel!(args, toml, log, starttime, (), false)?,
-                  first: *args.get_one("first").unwrap_or(&usize::MAX),
-                  last: *args.get_one("last").unwrap_or(&usize::MAX) })
+    fn try_new(cli: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
+        Ok(Self { show: sel!(cli, toml, log, show, "musa", Show::m())?,
+                  search: cli.get_many("search").unwrap_or_default().cloned().collect(),
+                  exact: cli.get_flag("exact"),
+                  starttime: sel!(cli, toml, log, starttime, (), false)?,
+                  first: *cli.get_one("first").unwrap_or(&usize::MAX),
+                  last: *cli.get_one("last").unwrap_or(&usize::MAX) })
     }
 }
 
 impl ConfPred {
-    fn try_new(args: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
-        let tmpdirs = if let Some(a) = args.get_many::<PathBuf>("tmpdir") {
+    fn try_new(cli: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
+        let tmpdirs = if let Some(a) = cli.get_many::<PathBuf>("tmpdir") {
             a.cloned().collect()
         } else if let Some(a) = toml.predict.as_ref().and_then(|t| t.tmpdir.as_ref()) {
             a.to_vec()
         } else {
             vec![PathBuf::from("/var/tmp")]
         };
-        Ok(Self { show: sel!(args, toml, predict, show, "emta", Show::emt())?,
-                  avg: sel!(args, toml, predict, avg, (), Average::Median)?,
-                  lim: sel!(args, toml, predict, limit, 1..65000, 10)? as u16,
-                  unknown: sel!(args, toml, predict, unknown, 0..3600, 10)?,
-                  resume: *args.get_one("resume").unwrap_or(&ResumeKind::Current),
+        Ok(Self { show: sel!(cli, toml, predict, show, "emta", Show::emt())?,
+                  avg: sel!(cli, toml, predict, avg, (), Average::Median)?,
+                  lim: sel!(cli, toml, predict, limit, 1..65000, 10)? as u16,
+                  unknown: sel!(cli, toml, predict, unknown, 0..3600, 10)?,
+                  resume: *cli.get_one("resume").unwrap_or(&ResumeKind::Current),
                   tmpdirs,
-                  first: *args.get_one("first").unwrap_or(&usize::MAX),
-                  last: *args.get_one("last").unwrap_or(&usize::MAX) })
+                  first: *cli.get_one("first").unwrap_or(&usize::MAX),
+                  last: *cli.get_one("last").unwrap_or(&usize::MAX) })
     }
 }
 
 impl ConfStats {
-    fn try_new(args: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
-        Ok(Self { show: sel!(args, toml, stats, show, "ptsa", Show::p())?,
-                  search: args.get_many("search").unwrap_or_default().cloned().collect(),
-                  exact: args.get_flag("exact"),
-                  lim: sel!(args, toml, stats, limit, 1..65000, 10)? as u16,
-                  avg: sel!(args, toml, stats, avg, (), Average::Median)?,
-                  group: sel!(args, toml, stats, group, (), Timespan::None)? })
+    fn try_new(cli: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
+        Ok(Self { show: sel!(cli, toml, stats, show, "ptsa", Show::p())?,
+                  search: cli.get_many("search").unwrap_or_default().cloned().collect(),
+                  exact: cli.get_flag("exact"),
+                  lim: sel!(cli, toml, stats, limit, 1..65000, 10)? as u16,
+                  avg: sel!(cli, toml, stats, avg, (), Average::Median)?,
+                  group: sel!(cli, toml, stats, group, (), Timespan::None)? })
     }
 }
 
 impl ConfAccuracy {
-    fn try_new(args: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
-        Ok(Self { show: sel!(args, toml, accuracy, show, "mta", Show::mt())?,
-                  search: args.get_many("search").unwrap_or_default().cloned().collect(),
-                  exact: args.get_flag("exact"),
-                  avg: sel!(args, toml, accuracy, avg, (), Average::Median)?,
-                  lim: sel!(args, toml, accuracy, limit, 1..65000, 10)? as u16,
-                  last: *args.get_one("last").unwrap_or(&usize::MAX) })
+    fn try_new(cli: &ArgMatches, toml: &Toml) -> Result<Self, Error> {
+        Ok(Self { show: sel!(cli, toml, accuracy, show, "mta", Show::mt())?,
+                  search: cli.get_many("search").unwrap_or_default().cloned().collect(),
+                  exact: cli.get_flag("exact"),
+                  avg: sel!(cli, toml, accuracy, avg, (), Average::Median)?,
+                  lim: sel!(cli, toml, accuracy, limit, 1..65000, 10)? as u16,
+                  last: *cli.get_one("last").unwrap_or(&usize::MAX) })
     }
 }
