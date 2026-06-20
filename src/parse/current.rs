vincentdephily/emlop

commit a19a85ccb6cd5b6608b845af5b009c4316a47f57

proces: Refactor Proc formating
* Formatting/editing of commandline is delayed until display time (potentially saving a few cycles)
* Display width is now user-configurable (should be in the `predict` section only, but that's a
  refactoring for another day)
* Switched from std `Display` to our `Disp` trait

That last part is the initial motivation, as I want to be able to pass an `indent` argument.

src/parse/current.rs
@@ -150,12 +150,10 @@ pub struct EmergeInfo {
 ///   should be the case for almost all users)
 pub fn get_emerge(tmpdirs: &mut Vec<PathBuf>) -> EmergeInfo {
     let mut res = EmergeInfo { start: i64::MAX, cmds: vec![], pkgs: vec![] };
-    let re_python = Regex::new("^[a-z/-]+python[0-9.]* [a-z/-]+python[0-9.]*/").unwrap();
-    for mut proc in get_all_proc(tmpdirs) {
+    for proc in get_all_proc(tmpdirs) {
         match proc.kind {
             ProcKind::Emerge => {
                 res.start = std::cmp::min(res.start, proc.start);
-                proc.cmdline = re_python.replace(&proc.cmdline, "").to_string();
                 res.cmds.push(proc);
             },
             ProcKind::Python => {

src/parse/proces.rs
@@ -6,12 +6,14 @@
 //! implementaion (does procinfo crate work on BSDs ?), but it's unit-tested against ps and should
 //! be fast.
 
-use crate::*;
+use crate::{table::Disp, *};
 use anyhow::{ensure, Context};
 use libc::pid_t;
+use regex::Regex;
 use std::{fs::{read_dir, DirEntry, File},
           io::prelude::*,
-          path::PathBuf};
+          path::PathBuf,
+          sync::OnceLock};
 
 #[derive(Debug)]
 pub enum ProcKind {
@@ -29,18 +31,30 @@ pub struct Proc {
     pub ppid: pid_t,
 }
 
-impl std::fmt::Display for Proc {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let pid = format!("Pid {}: ", self.pid);
-        let capacity = f.precision().unwrap_or(45).saturating_sub(pid.len());
-        let cmdlen = self.cmdline.len();
-        if capacity >= cmdlen || cmdlen < 4 {
-            write!(f, "{pid}{}", &self.cmdline)
-        } else if capacity > 3 {
-            write!(f, "{pid}...{}", &self.cmdline[(cmdlen - capacity + 3)..])
+/// Rewrite "/usr/bin/python312 /foo/bar/python312/emerge" as "emerge"
+// TODO: generalize to other cases, we just want the basename of the script/binary
+// TODO: msrv 1.80: switch to LazyLock
+static RE_PROC: OnceLock<Regex> = OnceLock::new();
+
+pub struct FmtProc<'a>(pub &'a Proc);
+impl Disp for FmtProc<'_> {
+    fn out(&self, buf: &mut Vec<u8>, conf: &Conf) -> usize {
+        let start = buf.len();
+        let prefixlen = (self.0.pid.max(1).ilog10() + 2) as usize;
+        let re_proc = RE_PROC.get_or_init(|| {
+    Regex::new("^[a-z/-]+(python|bash|sandbox)[0-9.]* [a-z/-]+python[0-9.]*/").unwrap()
+});
+        let cmdline = re_proc.replace(self.0.cmdline.replace('\0', " ").trim(), "").to_string();
+        let cmdcap = conf.procwidth.saturating_sub(prefixlen);
+        let cmdlen = cmdline.len();
+        if cmdcap >= cmdlen {
+            wtb!(buf, "{} {}", self.0.pid, cmdline)
+        } else if cmdcap > 3 {
+            wtb!(buf, "{} ...{}", self.0.pid, &cmdline[(cmdlen - cmdcap + 3)..])
         } else {
-            write!(f, "{pid}...")
+            wtb!(buf, "{} ...", self.0.pid)
         }
+        buf.len() - start
     }
 }
 
@@ -74,7 +88,6 @@ fn get_proc(entry: &DirEntry,
     // Parse arguments
     let mut cmdline = String::new();
     File::open(entry.path().join("cmdline")).ok()?.read_to_string(&mut cmdline).ok()?;
-    cmdline = cmdline.replace('\0', " ").trim().into();
     // Done
     Some(Proc { kind, cmdline, start: time_ref + start_time / clocktick, pid, ppid })
 }
@@ -133,7 +146,7 @@ fn get_all_proc_result(tmpdirs: &mut Vec<PathBuf>) -> Result<Vec<Proc>, Error> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{config::Conf, *};
     use regex::Regex;
     use std::{collections::BTreeMap, process::Command};
     use time::{macros::format_description, PrimitiveDateTime};
@@ -204,43 +217,33 @@ mod tests {
     }
 
     #[test]
-    fn format_info() {
-        let s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        let t: Vec<(i32, usize, usize, &str)> = vec![// Precison is way too small, use elipsis starting at 4 chars
-                                                     (1, 1, 1, "Pid 1: a"),
-                                                     (1, 2, 1, "Pid 1: ab"),
-                                                     (2, 3, 1, "Pid 2: abc"),
-                                                     (3, 4, 1, "Pid 3: ..."),
-                                                     (4, 5, 1, "Pid 4: ..."),
-                                                     (330, 1, 1, "Pid 330: a"),
-                                                     (331, 2, 1, "Pid 331: ab"),
-                                                     (332, 3, 1, "Pid 332: abc"),
-                                                     (333, 4, 1, "Pid 333: ..."),
-                                                     (334, 5, 1, "Pid 334: ..."),
-                                                     // Here we have enough space
-                                                     (1, 1, 12, "Pid 1: a"),
-                                                     (1, 2, 12, "Pid 1: ab"),
-                                                     (1, 3, 12, "Pid 1: abc"),
-                                                     (1, 4, 12, "Pid 1: abcd"),
-                                                     (1, 5, 12, "Pid 1: abcde"),
-                                                     (12, 4, 12, "Pid 12: abcd"),
-                                                     (123, 3, 12, "Pid 123: abc"),
-                                                     (1234, 2, 12, "Pid 1234: ab"),
-                                                     // Running out of space again, but we can display part of it
-                                                     (1, 6, 12, "Pid 1: ...ef"),
-                                                     (1, 7, 12, "Pid 1: ...fg"),
-                                                     (1, 8, 12, "Pid 1: ...gh"),
-                                                     (22, 9, 12, "Pid 22: ...i"),];
-        for (pid, cmdlen, precision, out) in t.into_iter() {
-            dbg!((pid, cmdlen, precision, out));
-            let i = Proc { kind: ProcKind::Other,
+    #[rustfmt::skip]
+    fn format_proc() {
+        let t: Vec<(i32, usize, &str)> = vec![// Here we have enough space
+                                              (1,       1, "1 1"),
+                                              (1,       2, "1 12"),
+                                              (1,       8, "1 12345678"),
+                                              (12,      7, "12 1234567"),
+                                              (123,     6, "123 123456"),
+                                              (1234,    5, "1234 12345"),
+                                              // Running out of space, but we can display part of it
+                                              (1,      10, "1 ...67890"),
+                                              (12345,  10, "12345 ...0"),
+                                              // Capacity is way too small, use elipsis starting at 4 chars
+                                              (1234567, 3, "1234567 ..."),
+                                              (123456,  3, "123456 123"),
+        ];
+        for (pid, cmdlen, out) in t.into_iter() {
+            dbg!((pid, cmdlen, out));
+            let conf = Conf::from_str(&format!("emlop p --procwidth 10"));
+            let mut buf = vec![];
+            let p = Proc { kind: ProcKind::Other,
                            pid,
                            ppid: -1,
-                           cmdline: s[..cmdlen].to_string(),
+                           cmdline: "1234567890"[..cmdlen].to_string(),
                            start: 0 };
-            let f = format!("{1:.0$}", precision, i);
-            assert!(precision < 10 || f.len() <= precision, "{} <= {}", f.len(), precision);
-            assert_eq!(f, out);
+            FmtProc(&p).out(&mut buf, &conf);
+            assert_eq!(String::from_utf8(buf), Ok(String::from(out)));
         }
     }
 }

completion.bash
@@ -167,7 +167,7 @@ _emlop() {
             return 0
             ;;
         emlop__predict)
-            opts="-s -N -n -f -t -H -o -F -v -h --show --first --last --tmpdir --resume --unknown --avg --limit --from --to --header --duration --date --utc --color --output --logfile --help"
+            opts="-s -N -n -f -t -H -o -F -v -h --show --first --last --tmpdir --resume --unknown --avg --limit --from --to --header --duration --date --utc --color --output --procwidth --logfile --help"
             if [[ ${cur} == -* ]] ; then
                 COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                 return 0
@@ -190,6 +190,9 @@ _emlop() {
                     ;;
                 --color)
                     COMPREPLY=($(compgen -W "yes no auto" "${cur}"))
+                    ;;
+                --procwidth)
+                    COMPREPLY=($(compgen -W "10 20 40 80 160" "${cur}"))
                     ;;
                 --output|-o)
                     COMPREPLY=($(compgen -W "tab columns auto" "${cur}"))

completion.fish
@@ -34,6 +34,7 @@ complete -c emlop -n "__fish_seen_subcommand_from predict" -l resume -d 'Use mai
 complete -c emlop -n "__fish_seen_subcommand_from predict" -l unknown -d 'Assume unkown packages take <secs> seconds to merge' -x -a "0 5 10 20 60"
 complete -c emlop -n "__fish_seen_subcommand_from predict" -l avg -d 'Select function used to predict durations' -x -a "arith median weighted-arith weighted-median"
 complete -c emlop -n "__fish_seen_subcommand_from predict" -l limit -d 'Use the last <num> merge times to predict durations' -x -a "1 5 20 999"
+complete -c emlop -n "__fish_seen_subcommand_from predict" -l procwidth -d 'Maximum display width for emerge process' -x -a "10 20 40 80 160"
 
 complete -c emlop -n "__fish_seen_subcommand_from stats" -s s -l show -d 'Show (p)ackages, (t)otals, (s)yncs, and/or (a)ll' -x -a "ptsa"
 complete -c emlop -n "__fish_seen_subcommand_from stats" -s g -l groupby -d 'Group by (y)ear, (m)onth, (w)eek, (d)ay, (n)one' -x -a "year month week day none"

completion.zsh
@@ -87,6 +87,7 @@ _emlop() {
 '--color=[Enable color (yes/no/auto)]' \
 '-o+[Ouput format (columns/tab/auto)]:format: ' \
 '--output=[Ouput format (columns/tab/auto)]:format: ' \
+'--procwidth=[Maximum display width for emerge process]'
 '-F+[Location of emerge log file]:file: ' \
 '--logfile=[Location of emerge log file]:file: ' \
 '*-v[Increase verbosity (can be given multiple times)]' \

emlop.toml
@@ -12,6 +12,7 @@
 # header = true
 # color = "yes"
 # output = "columns"
+# procwidth = 60
 [log]
 # show = "mus"
 # starttime = true

src/commands.rs
@@ -304,7 +304,7 @@ pub fn cmd_predict(gc: &Conf, sc: &ConfPred) -> Result<bool, Error> {
     }
     if sc.show.emerge {
         for proc in &einfo.cmds {
-            tbl.row([&[&proc], &[&FmtDur(now - proc.start)], &[]]);
+            tbl.row([&[&FmtProc(proc)], &[&FmtDur(now - proc.start)], &[]]);
         }
     }
 

src/config.rs
@@ -37,6 +37,7 @@ pub struct Conf {
     pub logfile: String,
     pub from: Option<i64>,
     pub to: Option<i64>,
+    pub procwidth: usize,
 }
 pub struct ConfLog {
     pub show: Show,
@@ -164,6 +165,7 @@ impl Conf {
                   dur_t: sel!(cli, toml, duration, (), DurationStyle::Hms)?,
                   date_offset: offset,
                   date_fmt: sel!(cli, toml, date, (), DateStyle::default())?,
+                  procwidth: sel!(cli, toml, predict, procwidth, 10..1000, 60)? as usize,
                   out: sel!(cli, toml, output, isterm, outdef)? })
     }
     #[cfg(test)]

commit 86cb37a0feeedeee0501f54069b5520792cdbfc6

process: Improve detection of sandbox processes
* Handle case where program name is actually "sandbox", without a
  python wrapper
* Better handling of sandbox arg (use the \0 separator), fixing a
  regression from a19a85ccb6cd5b6608b845af5b009c4316a47f57

src/parse/current.rs
@@ -251,10 +251,10 @@ pub fn get_emerge(procs: &ProcList) -> EmergeInfo {
                 res.start = std::cmp::min(res.start, proc.start);
                 res.roots.push(*pid);
             },
-            ProcKind::Python => {
-                if let Some(a) = proc.cmdline.find("sandbox [") {
-                    if let Some(b) = proc.cmdline.find("] sandbox") {
-                        if let Some(p) = Pkg::try_new(&proc.cmdline[(a + 9)..b], false) {
+            ProcKind::Sandbox => {
+                if let Some(a) = proc.cmdline.find("] sandbox\0") {
+                    if let Some(b) = proc.cmdline[..a].rfind("[") {
+                        if let Some(p) = Pkg::try_new(&proc.cmdline[(b + 1)..a], false) {
                             res.pkgs.push(p);
                         }
                     }

src/parse/proces.rs
@@ -18,7 +18,7 @@ use std::{collections::BTreeMap,
 #[derive(Debug, Clone, Copy)]
 pub enum ProcKind {
     Emerge,
-    Python,
+    Sandbox,
     Other,
 }
 
@@ -101,8 +101,10 @@ fn get_proc(entry: &DirEntry,
     let kind = if &stat[cmd_start..cmd_end] == "emerge" {
         extend_tmpdirs(entry.path(), tmpdirs);
         ProcKind::Emerge
-    } else if stat[cmd_start..cmd_end].starts_with("python") {
-        ProcKind::Python
+    } else if stat[cmd_start..cmd_end].starts_with("python")
+              || stat[cmd_start..cmd_end] == *"sandbox"
+    {
+        ProcKind::Sandbox
     } else {
         ProcKind::Other
     };